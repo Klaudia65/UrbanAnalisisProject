720e7a3fe1fa011ee86f203b3cd4288f4bd1c6f4
Klaudia65 <klaudia.kubale@etudiant.univ-lr.fr>
2024-11-22 23:53:10 +0900

    enlarging data

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load the data
 data = pd.read_csv('data/all_data.csv')
-
-# Explore the data
-print(data.head())
-print(data.info())
+data['city'] = 'NYC' 
+data.loc[43:, 'city'] = 'S'
+print(data)
 print(data.describe())
 
 # Clean the data
 data = data.dropna()  # Remove rows with missing values
 
-# Analyze the data
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-sns.pairplot(data)
-plt.show()
-
-# Prepare the data
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-
-X = data.drop('target_column', axis=1)  # Replace 'target_column' with the name of your target column
-y = data['target_column']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+print(data.shape)
